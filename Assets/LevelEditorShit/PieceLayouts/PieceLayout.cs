10a33536a23cda39b012b53caa375b848bca219b
Matt <mattbrabham@hotmail.com>
2020-12-11 17:00:39 +0000

    Piece Instantiation on load

---
+++ Assets/LevelEditorShit/PieceLayouts/PieceLayout.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PieceLayout : ScriptableObject
+{
+    [System.Serializable]
+    public struct PieceAndPos
+    {
+        public GameObject Piece;
+        public Vector2 Location;
+    }
+
+    public PieceAndPos[] LayoutsPieces1;
+    public int colour1;
+    public int direction1;
+
+    public PieceAndPos[] LayoutsPieces2;
+    public int colour2;
+    public int direction2;
+}
---
+++ Assets/Scripts/Management/PieceInstantiatorScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceInstantiatorScript : MonoBehaviour
+{
+    public PieceLayout layout;
+    private BoardHandlerScript BHS;
+
+    void Start()
+    {
+        BHS = GameObject.Find("BoardHandler").GetComponent<BoardHandlerScript>();
+        for (int i = 0; i < layout.LayoutsPieces1.Length; i++)
+        {
+            GameObject piece = Instantiate(layout.LayoutsPieces1[i].Piece, new Vector3(layout.LayoutsPieces1[i].Location.x, 1, layout.LayoutsPieces1[i].Location.y), layout.LayoutsPieces1[i].Piece.transform.rotation);
+            piece.transform.SetParent(BHS.transform);
+            piece.GetComponent<Piece>().colour = layout.colour1;
+            if (piece.GetComponent<Pawn>())
+            {
+                piece.GetComponent<Pawn>().direction = layout.direction1;
+            }
+            if (piece.GetComponent<King>())
+            {
+                piece.GetComponent<King>().direction = layout.direction1;
+            }
+        }
+
+        for (int z = 0; z < layout.LayoutsPieces2.Length; z++)
+        {
+            GameObject piece = Instantiate(layout.LayoutsPieces2[z].Piece, new Vector3(layout.LayoutsPieces2[z].Location.x, 1, layout.LayoutsPieces2[z].Location.y), layout.LayoutsPieces2[z].Piece.transform.rotation);
+            piece.transform.SetParent(BHS.transform);
+            piece.GetComponent<Piece>().colour = layout.colour2;
+            if (piece.GetComponent<Pawn>())
+            {
+                piece.GetComponent<Pawn>().direction = layout.direction2;
+            }
+            if (piece.GetComponent<King>())
+            {
+                piece.GetComponent<King>().direction = layout.direction2;
+            }
+        }
+    }
+}
