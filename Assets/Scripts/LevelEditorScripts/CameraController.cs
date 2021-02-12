1ab1d2c283a42950d65904c99ff8dd9655a0fe69
Matt <mattbrabham@hotmail.com>
2021-02-12 23:23:42 +0000

    Camera Controller for Level Editor

---
+++ Assets/Scripts/LevelEditorScripts/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float scrollSense;
+    public float panSense;
+
+    public float maxZoom, minZoom;
+    public float zoom;
+
+    private bool cameraMove;
+
+    public float cursorMoveStartX, cursorMoveStartY;
+
+    void Start()
+    {
+        zoom = 5;
+    }
+
+    void Update()
+    {
+        zoom += -Input.GetAxis("Mouse ScrollWheel") * scrollSense;
+        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+        this.GetComponent<Camera>().orthographicSize = zoom;
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            cameraMove = true;
+            cursorMoveStartX = Input.mousePosition.x / Screen.width;
+            cursorMoveStartY = Input.mousePosition.y / Screen.height;
+        }
+        else if (Input.GetMouseButtonUp(2)){
+            cameraMove = false;
+        }
+
+        if (cameraMove)
+        {
+            float currentCursorX, currentCursorY;
+            currentCursorX = Input.mousePosition.x / Screen.width;
+            currentCursorY = Input.mousePosition.y / Screen.height;
+
+            this.transform.Translate(new Vector3((cursorMoveStartX - currentCursorX) * -panSense, (cursorMoveStartY - currentCursorY) * -panSense, 0));
+        }
+    }
+}
