f692dff52181686cc4ae9bb6027a251ca7dc9a29
Matt <mattbrabham@hotmail.com>
2021-02-13 13:53:32 +0000

    Board drawer complete

--- Assets/Scripts/LevelEditorScripts/CurrentBoardHandler.cs
+++ Assets/Scripts/LevelEditorScripts/CurrentBoardHandler.cs
@@ -4,23 +4,50 @@ using UnityEngine;
 
 public class CurrentBoardHandler : MonoBehaviour
 {
-    public bool[,] Cb = new bool[32, 32];
+    public struct ChessSquare
+    {
+        public bool isActive;
+        public GameObject reference;
+    }
+
+    public ChessSquare[,] Cb = new ChessSquare[32, 32];
 
     public Material lightSquare, darkSquare;
     public GameObject squareSprite;
 
     void Start()
     {
-        DrawSquare(lightSquare, new Vector2(0, 0));
+
     }
 
     public void RedrawBoard()
     {
-       
+        for (int file = 0; file < 32; file++){
+            for(int rank = 0; rank < 32; rank++){
+                if (Cb[rank,file].isActive == false && Cb[rank, file].reference == null) continue;
+                if (Cb[rank, file].isActive == true && Cb[rank, file].reference != null) continue;
+
+                if (Cb[rank, file].isActive == true && Cb[rank, file].reference == null)
+                {
+                    bool isLightSquare = (file + rank) % 2 != 0;
+                    Material squareColour = (isLightSquare) ? lightSquare : darkSquare;
+                    Vector2 position = new Vector2(file, rank);
+                    Cb[rank, file].isActive = true;
+                    Cb[rank, file].reference = DrawSquare(squareColour, position);
+                }
+                else if(Cb[rank, file].isActive == false && Cb[rank, file].reference != null)
+                {
+                    Destroy(Cb[rank, file].reference);
+                    Cb[rank, file].reference = null;
+                }
+            }
+       }
     }
 
-    public void DrawSquare(Material colour, Vector2 pos)
+    public GameObject DrawSquare(Material colour, Vector2 pos)
     {
-        GameObject square = Instantiate(squareSprite, pos, Quaternion.Euler(90, 0, 0), this.transform);
+        GameObject square = Instantiate(squareSprite, new Vector3(pos.x,0,pos.y), Quaternion.Euler(-90, 0, 0), this.transform);
+        square.GetComponent<SpriteRenderer>().material = colour;
+        return square.gameObject;
     }
 }
--- Assets/Scripts/LevelEditorScripts/LevelEditorCursor.cs
+++ Assets/Scripts/LevelEditorScripts/LevelEditorCursor.cs
@@ -7,18 +7,22 @@ public class LevelEditorCursor : MonoBehaviour
     public int cursorPosX;
     public int cursorPosY;
 
-
+    public CurrentBoardHandler CBH;
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        cursorPosX = Mathf.RoundToInt(Input.mousePosition.x);
-        cursorPosY = Mathf.RoundToInt(Input.mousePosition.y);
+        cursorPosX = Mathf.RoundToInt(this.transform.position.x);
+        cursorPosY = Mathf.RoundToInt(this.transform.position.z);
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y));
 
 
+
+        //CBH.Cb[cursorPosY, cursorPosX].isActive = true;
+        //CBH.RedrawBoard();
     }
 }
